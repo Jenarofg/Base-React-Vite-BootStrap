97bae11953557a391496d98ed209a444638c217f
Jenarofg <jenarofg@gmail.com>
2024-10-29 19:53:48 +0100

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() { 
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header/>
+      <Home />
+      
+    </div>
+  )
+}
+
+export default App
